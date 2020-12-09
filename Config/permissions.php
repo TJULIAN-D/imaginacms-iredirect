48e744510ba0913154e3ecf123a81a050426d0c3
Juan Escalante <jcec007@gmail.com>
2020-12-09 18:17:30 -0500

    +Add: Manage Permission for the qadmin

--- Config/permissions.php
+++ Config/permissions.php
@@ -3,6 +3,7 @@
 return [
 
     'iredirect.redirects' => [
+        'manage' => 'iredirect::redirect.manage',
         'index' => 'iredirect::redirect.list',
         'create' => 'iredirect::redirect.create',
         'edit' => 'iredirect::redirect.edit',
